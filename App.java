9c1dc2865351af0397437c5b4c9f9b8d1d9dcd6b
Sara Ibarra <saraiba33@gmail.com>
2022-05-24 11:09:05 -0600

    Add solution

--- App.java
+++ App.java
@@ -1,14 +1,31 @@
 class App {
-  public static void main(String args[]){
+  public static void main(String args[]) {
     App.forLoop();
     App.whileLoop();
     App.doWhileLoop();
   }
 
   private static void forLoop() {
+    String message = "Launch!";
+    for (int i = 10; i >= 1; i--) {
+      System.out.println(i);
+    }
+    System.out.println(message);
   }
+
   private static void whileLoop() {
+    int index = 0;
+    while (index < 5) {
+      System.out.println("I love Java");
+      index++;
+    }
   }
+
   private static void doWhileLoop() {
+    int index = 0;
+    do {
+      System.out.println("I love Java");
+      index++;
+    } while (index < 5);
   }
 }
